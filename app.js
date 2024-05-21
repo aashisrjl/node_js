bc726082381e2e4b03644693e9ccb973904d9f8f
Aashis Rijal <aashisrijal252@gmail.com>
2024-05-21 21:25:27 +0545

    update readme

--- app.js
+++ app.js
@@ -5,17 +5,15 @@ const port = 3000;
 
 app.set("view engine", "ejs")
 
-
-
 app.get('/',(req,res)=>{
 
     res.render('home.ejs');// address:address
-
 })
 
 app.get('/login',(req,res)=>{
     res.render('auth/login.ejs');
 })
+
 app.get('/register',(req,res)=>{
     res.render('auth/register.ejs');
 })
